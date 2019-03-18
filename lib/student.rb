8ce2f39ca150a92631dfb6b1f5267ed7aded54f9
Riccardo <riccardo_london@live.it>
2019-03-18 11:16:43 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,7 +3,7 @@ class Student < ChattyStudent
 
 def hello
   super
-  return "Hey there! I'm so excited to learn stuff.\n"
+  return "Hey there! I'm so excited to learn stuff."
 end
 
 def raise_hand
